b51023d6e9c9417c00eb78a625bfb520cc4a538e
terry chay <tychay@php.net>
2009-04-17 13:42:45 +0000

    Issue 1: say hello world This says it, has a working prepend script, but that's it.

---
+++ samples/config.php
@@ -0,0 +1,15 @@
+<?php
+// vim:set expandtab tabstop=4 shiftwidth=4 softtabstop=4 foldmethod=marker syntax=php:
+//345678901234567890123456789012345678901234567890123456789012345678901234567890
+/**
+ * Initialize the configuration on every page in the TGIFramework samples. This is
+ * called automatically on the page.
+ *
+ * @package tgiframework
+ * @subpackage samples
+ * @copyright 2009 terry chay <tychay@php.net>
+ * @license GNU Lesser General Public License <http://www.gnu.org/licenses/lgpl.html>
+ * @author terry chay <tychay@tagged.com>
+ */
+?>
+CONFIG LOADED
