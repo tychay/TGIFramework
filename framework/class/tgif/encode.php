e47f8274f69200b161afc057547741e23cb27d36
terry chay <tychay@php.net>
2011-03-31 05:11:03 +0000

    syntax error

--- framework/class/tgif/encode.php
+++ framework/class/tgif/encode.php
@@ -116,11 +116,12 @@ class tgif_encode {
         $alt = time() % 2;
         for ($i = 0; $i < $length; $i++) {
             if ($alt == 1) {
-            $password .= $consonants[(rand() % strlen($consonants))];
-            $alt = 0;
-        } else {
-            $password .= $vowels[(rand() % strlen($vowels))];
-            $alt = 1;
+                $password .= $consonants[(rand() % strlen($consonants))];
+                $alt = 0;
+            } else {
+                $password .= $vowels[(rand() % strlen($vowels))];
+                $alt = 1;
+            }
         }
         return $password;
     }
