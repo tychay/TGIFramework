6d3b8e8c929748af2dbd24fa7d6b9c8153037caa
terry chay <tychay@php.net>
2011-03-31 05:04:03 +0000

    password generator

--- framework/class/tgif/encode.php
+++ framework/class/tgif/encode.php
@@ -88,6 +88,43 @@ class tgif_encode {
         return str_pad($num64, $length, '0', STR_PAD_LEFT);
     }
     // }}}
+    // {{{ + create_password($str)
+    /**
+     * Simple password generator.
+     *
+     * Taken from http://www.webtoolkit.info/php-random-password-generator.html.
+     *
+     * @return string
+     **/
+    public static function create_password($length=9,$stringth=0)
+    {
+        $vowels = 'aeuy';
+        $consonants = 'bdghjmnpqrstvz';
+        if ($strength & 1) {
+            $consonants .= 'BDGHJLMNPQRSTVWXZ';
+        }
+        if ($strength & 2) {
+            $vowels .= "AEUY";
+        }
+        if ($strength & 4) {
+            $consonants .= '23456789';
+        }
+        if ($strength & 8) {
+            $consonants .= '@#$%';
+        }
+        $password = '';
+        $alt = time() % 2;
+        for ($i = 0; $i < $length; $i++) {
+            if ($alt == 1) {
+            $password .= $consonants[(rand() % strlen($consonants))];
+            $alt = 0;
+        } else {
+            $password .= $vowels[(rand() % strlen($vowels))];
+            $alt = 1;
+        }
+        return $password;
+    }
+    // }}}
     // Integer manipulations
     // {{{ + int_to_base64($number)
     /**
